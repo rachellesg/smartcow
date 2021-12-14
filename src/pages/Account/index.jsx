ed5825bb409e7fc280d06a448603b1e7a9c3a500
Rachelle <rachelle.yeo@outlook.com>
2021-12-14 14:32:00 +0800

    feat: add tabs to Account

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,43 +1,20 @@
-import { useState } from "react";
-
 // Pages
+
+// Elements
+import Tabs from "../../elements/Tabs";
 
 // Style
 import { AccountWrapper } from "./styles";
 
 function Account() {
-  const [selectTab, setSelectTab] = useState("Profile");
-
-  const pages = ["Profile", "Plan", "Billing"];
-
-  const selectedAccountTabs = (page) => {
-    console.log(page);
-    setSelectTab(page);
-  };
+  const pages = [
+    { name: "Profile", content: "Profile XX" },
+    { name: "Plan", content: "Plan C" },
+    { name: "Billing", content: "Billing NN" },
+  ];
   return (
     <AccountWrapper>
-      <ul className="tabs">
-        {pages.map((page) => {
-          return (
-            <li
-              key={page}
-              className={`tab ${selectTab === page ? "active" : ""}`}
-              onClick={() => {
-                selectedAccountTabs(page);
-              }}
-            >
-              {page}
-            </li>
-          );
-        })}
-      </ul>
-      <div className="tabs-content">
-        {selectTab === "Profile"
-          ? "if hits profile"
-          : selectTab === "Plan"
-          ? "hits plan"
-          : "others billing"}
-      </div>
+      <Tabs pages={pages} />
     </AccountWrapper>
   );
 }
--- src/pages/Account/styles.jsx
+++ src/pages/Account/styles.jsx
@@ -1,3 +1,4 @@
 import styled from "styled-components";
 
-export const AccountWrapper = styled.div``;
+export const AccountWrapper = styled.div`
+`;
