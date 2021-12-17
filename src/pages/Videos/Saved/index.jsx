c5859235f4470d07f4701488ed9c323be427265d
Rachelle <rachelle.yeo@outlook.com>
2021-12-17 14:13:54 +0800

    feat: update saved videos

--- src/pages/Videos/Saved/index.jsx
+++ src/pages/Videos/Saved/index.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+
+// Components
+import Header from "../../../components/Header";
+
+// Elements
+import { PrimaryButton } from "../../../elements/Button";
+import VideoCard from "../../../elements/Video";
+
+// Assets
+import personOne from "../../../assets/Videos/person-1.svg";
+
 function SavedVideos() {
-  return <>My Saved Videos xxx</>;
+  const pageHeaderDetails = {
+    pageTitle: "Saved Videos",
+    buttonsHeader: () => (
+      <PrimaryButton>
+        <Link to="/videos/create">Create New</Link>
+      </PrimaryButton>
+    ),
+  };
+  return (
+    <>
+      <Header pageHeaderDetails={pageHeaderDetails} />
+      <div className="col l3">
+        <VideoCard image={personOne} />
+      </div>
+    </>
+  );
 }
 
 export default SavedVideos;
