497656e57df955b49d020326cd5649b01c1f3d99
Rachelle <rachelle.yeo@outlook.com>
2021-12-20 16:30:07 +0800

    fix: move styling

--- src/pages/Account/styles.jsx
+++ src/pages/Account/styles.jsx
@@ -1,28 +1,3 @@
 import styled from "styled-components";
 
-export const AccountWrapper = styled.div`
-  /* overwrite materialize css, specifically for tabs in account only */
-  .tabs {
-    margin-bottom: 45px;
-    height: auto !important;
-    .tab {
-      text-align: left;
-      text-transform: none;
-      flex-grow: 0;
-      min-width: 80px;
-      font-size: 15px;
-      font-weight: 500;
-      line-height: 23px;
-      text-align: left;
-      height: auto !important;
-      color: #999999;
-      cursor: pointer;
-      &:hover {
-        color: #3860ad;
-      }
-      &.active {
-        color: #3860ad;
-      }
-    }
-  }
-`;
+export const AccountWrapper = styled.div``;
