9f1ea43713ff2c0d62bf6e3c5aba0f0577f0e6c2
Rachelle <rachelle.yeo@outlook.com>
2021-12-21 21:32:17 +0800

    fix: update accordion active style

--- src/elements/Accordion/index.jsx
+++ src/elements/Accordion/index.jsx
@@ -4,7 +4,7 @@ function Accordion({ title, content }) {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="accordion-item">
+    <div className={`accordion-item${isActive ? " active" : ""}`}>
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
         <div>{title}</div>
         <div>{isActive ? "-" : "+"}</div>
