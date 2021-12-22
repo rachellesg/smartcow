9b155d8f24752f8e32d79aaa13e361040db84e64
Rachelle <rachelle.yeo@outlook.com>
2021-12-22 16:31:21 +0800

    feat: update editable header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,53 @@
-import InputField from "../../elements/InputField";
+import { useState } from "react";
+
+import ChevronIcon from "../../assets/Icons/Chevron";
+import { PrimaryButton } from "../../elements/Button";
+
 import { HeaderWrapper } from "./styles";
 
 function Header({ pageHeaderDetails }) {
-  console.log(pageHeaderDetails);
   const { pageTitle, isEditable, buttonsHeader, cancelButton } =
     pageHeaderDetails;
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [videoTitle, setVideoTitle] = useState(pageTitle);
+  const editOnClick = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleSave = () => {
+    setIsEdit(!isEdit);
+  };
   return (
-    <HeaderWrapper className="header">
+    <HeaderWrapper className={`header ${isEditable ? " header-editable" : ""}`}>
       {isEditable ? (
-        <h1
-          onClick={() => {
-            console.log("let edit page title");
-          }}
-        >
-          {pageTitle}
-          {/* Chevron Icon here  */}
-        </h1>
+        <>
+          <h1 className={`${isEdit ? "header-modal" : ""}`}>
+            {isEdit ? (
+              <div className="input">
+                <input
+                  type="text"
+                  value={videoTitle}
+                  onChange={(e) => setVideoTitle(e.target.value)}
+                />
+                <textarea>
+                  Fusce quis magna vel ex pellentesque consequat sed et turpis.
+                  Vivamus bibendum rutrum euismod. Sed non sagittis est, semper
+                </textarea>
+                <div className="tags">
+                  <div className="tag">Email</div>
+                  <div className="tag">Marketing</div>
+                  <div className="tag">Greeting</div>
+                </div>
+                <PrimaryButton onClick={handleSave}>Save</PrimaryButton>
+              </div>
+            ) : (
+              <div onClick={editOnClick}>
+                {videoTitle} <ChevronIcon />
+              </div>
+            )}
+          </h1>
+        </>
       ) : (
         <h1>{pageTitle}</h1>
       )}
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -13,6 +13,65 @@ export const HeaderWrapper = styled.div`
     flex-direction: column;
   }
 
+  &.header-editable {
+    h1 {
+      svg {
+        transform: rotate(180deg);
+        color: #000000;
+        margin-left: 20px;
+      }
+      &.header-modal {
+        &:after {
+          content: "";
+          position: fixed;
+          top: 0;
+          left: 0;
+          background: rgb(255, 255, 255, 0.9);
+          width: 100vw;
+          height: 100vh;
+          z-index: 5;
+        }
+      }
+
+      .input {
+        position: relative;
+        z-index: 10;
+        input[type="text"] {
+          width: 350px;
+          font-size: 22px;
+          line-height: 33px;
+          font-weight: 500;
+
+          color: #3860ad;
+          border: 0;
+          &:focus {
+            box-shadow: 0 1px 0 0 #b3caf7;
+          }
+        }
+        .tags {
+          display: flex;
+          justify-content: flex-start;
+          align-items: start;
+          flex: 0 1 33%;
+          flex-wrap: wrap;
+          .tag {
+            color: #999999;
+            font-size: 12px;
+            background: #f4f4f4;
+            border-radius: 6px;
+            padding: 2px 8px;
+            border: 1px solid #999999;
+            margin-right: 5px;
+            margin-bottom: 5px;
+            &:last-child {
+              margin-right: 0;
+            }
+          }
+        }
+      }
+    }
+  }
+
   &:after {
     background: #000000;
     opacity: 0.1;
@@ -29,17 +88,6 @@ export const HeaderWrapper = styled.div`
 
     button {
       margin-left: 20px;
-    }
-  }
-
-  .header-editable {
-    .input-field input[type="text"] {
-      font-size: 22px;
-      line-height: 33px;
-      font-weight: 500;
-
-      color: #3860ad;
-      border: 0;
     }
   }
 `;
