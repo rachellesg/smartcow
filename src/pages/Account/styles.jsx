fd2a45893c73b8e0334507aa95970c796dd01d93
Rachelle <rachelle.yeo@outlook.com>
2021-12-23 14:27:24 +0800

    Updates

--- src/pages/Account/styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const AccountWrapper = styled.div`
-  .header {
-    @media screen and (max-width: 768px) {
-      flex-direction: row;
-    }
-  }
-`;
